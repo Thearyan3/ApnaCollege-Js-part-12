d7e43dd8fe82f301c62d6961b0d8af2cf1d3453b
Aryan <chaudharyaryan3456@gmail.com>
2025-02-04 23:57:45 +0530

    updated

--- app.js
+++ app.js
@@ -104,10 +104,16 @@
 //Our first API Request (using fetch)
 let url = "https://catfact.ninja/fact";
 
-fetch(url)
-.then((res) => {
-    console.log(res);
-})
-.catch((err) => {
-    console.log(err);
-})
+async function getFacts() {
+    try{
+        let res = await fetch(url);
+        let data = await res.json();
+        console.log(data.fact);
+        let res2 = await fetch(url);
+        let data2 = await res2.json();
+        console.log(data2.fact);
+    }catch(e){
+        console.log("error - ",e);
+    }
+  console.log('bye');
+}
